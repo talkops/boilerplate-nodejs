803ca18e149deb408a80e0241243bb2a2a7d90c0
bierdok <2333728+bierdok@users.noreply.github.com>
2025-03-25 11:35:50 +0100

    Upgrade SDK

--- index.mjs
+++ index.mjs
@@ -1,7 +1,3 @@
 import { Extension } from 'talkops'
-import pkg from './package.json' with { type: 'json' }
 
-const extension = new Extension()
-  .setName('Boilerplate NodeJS')
-  .setDockerRepository('ghcr.io/talkops/boilerplate-nodejs')
-  .setVersion(pkg.version)
+const extension = new Extension().setName('Boilerplate NodeJS')
