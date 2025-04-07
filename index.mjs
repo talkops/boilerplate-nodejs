60e83c46065edf7b8494037243bfbe1c2027125c
bierdok <2333728+bierdok@users.noreply.github.com>
2025-04-07 14:24:39 +0200

    Remove default category and default icon

--- index.mjs
+++ index.mjs
@@ -1,7 +1,4 @@
 import { Extension } from 'talkops'
 
 // Documentation: https://www.npmjs.com/package/talkops
-const extension = new Extension()
-  .setName('Boilerplate NodeJS')
-  .setCategory('creativity')
-  .setIcon('https://cdn.talkops.app/icons/default.png')
+const extension = new Extension().setName('Boilerplate NodeJS')
